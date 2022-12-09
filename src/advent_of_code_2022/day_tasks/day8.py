edec5f76313665fb5ce2225bd1df5bb77d0695fb
Damian Cyntler <damian@cyntler.com>
2022-12-09 22:58:39 +0100

    feat: add day8 part2

--- src/advent_of_code_2022/day_tasks/day8.py
+++ src/advent_of_code_2022/day_tasks/day8.py
@@ -1,5 +1,6 @@
 def get_result(tree_map: list[str]):
     visible_trees_sum = 0
+    highest_scenic_score = 0
 
     for y, line in enumerate(tree_map):
         for x, char in enumerate(line):
@@ -8,14 +9,20 @@ def get_result(tree_map: list[str]):
                 continue
 
             tree = int(char)
+            scenic_score_elements = {
+                "up": 0,
+                "down": 0,
+                "left": 0,
+                "right": 0,
+            }
 
-            trees_around_top = []
-            trees_around_bottom = []
+            trees_around_up = []
+            trees_around_down = []
             for y2, line in enumerate(tree_map):
                 if y2 <= y - 1:
-                    trees_around_top.append(int(line[x]))
+                    trees_around_up.append(int(line[x]))
                 elif y2 >= y + 1:
-                    trees_around_bottom.append(int(line[x]))
+                    trees_around_down.append(int(line[x]))
 
             trees_around_left = []
             trees_around_right = []
@@ -27,9 +34,12 @@ def get_result(tree_map: list[str]):
                         elif x2 >= x + 1:
                             trees_around_right.append(int(char2))
 
+            trees_around_up.reverse()
+            trees_around_left.reverse()
+
             for trees_around in [
-                trees_around_top,
-                trees_around_bottom,
+                trees_around_up,
+                trees_around_down,
                 trees_around_left,
                 trees_around_right,
             ]:
@@ -37,4 +47,26 @@ def get_result(tree_map: list[str]):
                     visible_trees_sum += 1
                     break
 
-    return visible_trees_sum
+            for dir, trees_around in [
+                ("up", trees_around_up),
+                ("down", trees_around_down),
+                ("left", trees_around_left),
+                ("right", trees_around_right),
+            ]:
+                for tree_around in trees_around:
+                    scenic_score_elements[dir] += 1
+                    if tree_around >= tree:
+                        break
+
+            print(tree, scenic_score_elements)
+
+            scenic_score_multipy_result = 1
+            for i in scenic_score_elements:
+                scenic_score_multipy_result = (
+                    scenic_score_multipy_result * scenic_score_elements[i]
+                )
+
+            if scenic_score_multipy_result > highest_scenic_score:
+                highest_scenic_score = scenic_score_multipy_result
+
+    return visible_trees_sum, highest_scenic_score
